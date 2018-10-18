27ec695479f870c729ff893da4e8a3af58d9db62
rachid mavulule <rmavulule@eleakim.co.mz>
2018-10-18 13:57:11 +0200

    variaveis e operadores, array, loop e estilo

--- 00_formacao-engengheiro-front-end/introducao-javascript/js/principal.js
+++ 00_formacao-engengheiro-front-end/introducao-javascript/js/principal.js
@@ -4,9 +4,27 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent="Aparecida Nutricista";
 
-var paciente=document.querySelector("#primeiro-paciente");
-var peso=paciente.querySelector(".info-peso").textContent;
-var altura=paciente.querySelector(".info-altura").textContent;
-var imc= peso / (altura * altura);
-paciente.querySelector(".info-imc").textContent=imc;
-console.log(peso + " - " + altura + " - " + imc);
+var pacientes=document.querySelectorAll(".paciente");
+for (var i = 0; i<pacientes.length; i++) {
+	var paciente=pacientes[i];
+	var peso=paciente.querySelector(".info-peso").textContent;
+	var altura=paciente.querySelector(".info-altura").textContent;
+
+	var tdPesoInvalido=false;
+	var tdAlturaInvalida=false;
+
+	if (peso<0||peso>1000) {
+		tdPesoInvalido=true;
+		paciente.querySelector(".info-imc").textContent="Peso invalido";
+		paciente.classList.add("paciente-invalido");
+	}
+	if(altura<0||altura>3){
+		tdAlturaInvalida=true;
+		paciente.querySelector(".info-imc").textContent="Altura invalida";
+		paciente.classList.add("paciente-invalido");
+	}
+	if (!tdAlturaInvalida && !tdPesoInvalido) {
+		var imc= peso / (altura * altura);
+		paciente.querySelector(".info-imc").textContent=imc.toFixed(2);
+	}
+}
